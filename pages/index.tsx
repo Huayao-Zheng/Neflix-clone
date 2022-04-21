27c1114f2fb726141bb96d7d165c447ed7159ca6
lonelybuddy <henryzheng92@gmail.com>
2022-04-21 17:44:10 -0500

    completed website ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,44 +20,85 @@ type Props = {
   documentaries: Movie[];
 };
 
+// const Home = ({
+//   netflixOriginals,
+//   trendingNow,
+//   topRated,
+//   actionMovies,
+//   comedyMovies,
+//   horrorMovies,
+//   romanceMovies,
+//   documentaries,
+// }: Props) => {
+//   return (
+//     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+//       <Head>
+//         <title>Home - Netflix</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <Header />
+
+//       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+//         <Banner netflixOriginals={netflixOriginals} />
+
+//         <section className="md:space-y-24">
+//           <Row title="Trending Now" movies={trendingNow} />
+//           <Row title="Top Rated" movies={topRated} />
+//           <Row title="Action Thrillers" movies={actionMovies} />
+//           <Row title="Comedies" movies={comedyMovies} />
+//           <Row title="Scary Movies" movies={horrorMovies} />
+//           <Row title="Romance Movies" movies={romanceMovies} />
+//           <Row title="Documentaries" movies={documentaries} />
+//         </section>
+//       </main>
+//       {/* Modal */}
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 const Home = ({
   netflixOriginals,
-  trendingNow,
-  topRated,
   actionMovies,
   comedyMovies,
+  documentaries,
   horrorMovies,
   romanceMovies,
-  documentaries,
+  topRated,
+  trendingNow,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]">
+    <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh]`}>
       <Head>
-        <title>Home - Netflix</title>
+        <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
 
-      <main className="realative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
 
-        <section>
+        <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
+
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
           <Row title="Romance Movies" movies={romanceMovies} />
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
     </div>
   );
 };
 
 export default Home;
+
+////////////////////////////////
 
 export const getServerSideProps = async () => {
   const [
