af6cf5a34997baacec35200cfb596f8e0df69678
lonelybuddy <henryzheng92@gmail.com>
2022-04-25 23:46:09 -0500

    checked

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -15,7 +15,7 @@ export const Banner = ({ netflixOriginals }: Props) => {
   const [movie, setMovie] = useState<Movie | null>(null);
   const [, setShowModal] = useRecoilState(modalState);
   const [, setCurrentMovie] = useRecoilState(movieState);
-
+  //
   useEffect(() => {
     setMovie(
       netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
